bc1b34488d908d4ba260362a5dfdc473aeeb21eb
baatheo <baatheo@gmail.com>
2020-05-21 10:31:35 +0200

    Initial commit

---
+++ build/precache-manifest.b494d83a6f5f86868dedd23508da8ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6512a9c033d0585bf36333fb67d56f7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dba813ee05f839f593ca",
+    "url": "/static/css/main.49c50bb6.chunk.css"
+  },
+  {
+    "revision": "c088c778ba818143c2b7",
+    "url": "/static/js/2.90fc5050.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.90fc5050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba813ee05f839f593ca",
+    "url": "/static/js/main.9f01062f.chunk.js"
+  },
+  {
+    "revision": "f66840197a3e356c7caf",
+    "url": "/static/js/runtime-main.959ed35d.js"
+  }
+]);
